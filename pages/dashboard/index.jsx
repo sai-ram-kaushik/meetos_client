4da224ddc1ee043c1f8e7ecf91ea6422d69c858b
Sai Ram Kaushik <77630069+sai-ram-kaushik@users.noreply.github.com>
2023-09-26 09:19:13 +0530

    changed the dashboard (#5)
    * updated with some changes in login page and home page
    
    * updated the login auth
    
    * updated require changes
    
    * changed dashboard

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -25,6 +25,7 @@ const Dasboard = () => {
 
             <div className="flex items-center justify-center">
               <div>
+                Upcoming Events
                 
               </div>
             </div>
